ea6984f6dece9aff6db0bac93a6751504d33c5b0
Salah Osman <osman144@umn.edu>
2022-03-28 23:07:51 -0500

    k subarray

---
+++ sliding-window/1-max-subarray-size-k.js
@@ -0,0 +1,29 @@
+// Easy
+// # Maximum Sum Subarray of Size K
+
+/*
+Input: [2, 1, 5, 1, 3, 2], k=3 
+Output: 9
+Explanation: Subarray with maximum sum is [5, 1, 3].
+
+Input: [2, 3, 4, 1, 5], k=2 
+Output: 7
+Explanation: Subarray with maximum sum is [3, 4].
+**/ 
+
+function maxSumSubarray(arr, k){
+  let maxSum = 0;
+  let windowSum = 0;
+  let windowStart = 0;
+
+  for(let windowEnd = 0; windowEnd < arr.length; windowEnd++){
+    windowSum = windowSum + arr[windowEnd] // 2
+
+    if(windowEnd >= k - 1){
+      maxSum = Math.max(maxSum, windowSum);
+      windowSum = windowSum - arr[windowStart];
+    }
+  }
+}
+
+console.log(maxSumSubarray([2, 3, 4, 1, 5], 2))
--- sliding-window/1-max-subarray-size-k.md
+++
@@ -1,2 +0,0 @@
-Easy
-# Maximum Sum Subarray of Size K
