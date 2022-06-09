1fb96e60b297ef76cded801e69e5ec26e874957c
Salah Osman <cvhashim04@gmail.com>
2022-06-08 21:51:11 -0500

    redo

--- 1-sliding-window/0-intro.js
+++ 1-sliding-window/0-intro.js
@@ -2,7 +2,7 @@
 // ============  average of all  subarrays of ‘K’ contiguous elements ==============
 
 /*
- Given an array, find the average of all  subarrays of ‘K’ contiguous elements in it.
+ Given an array, find the average of all subarrays of ‘K’ contiguous elements in it.
  return an array of the averages
 **/
 
@@ -10,57 +10,21 @@
   Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
 **/
 
-// O(n^2) solution
-
-// function average(input, k){
-//   let res = [];
-
-
-//   for(let i=0; i<input.length-k+1; i++){
-//     let sum=0;
-
-//     for(let j=i; j<i+k; j++){
-//       sum=sum+input[j];
-//     }
-//     res.push(sum/k) // calculate average
-//   }
-//   return res;
-// }
-
-// With a while loop
-// function average(input, k){
-//   let res = [];
-
-//   for(let left=0; left<input.length-k;left++){
-//     let sum = 0;
-//     let right = left;
-//     while(right<left+k){
-//       sum = sum + input[right];
-//       right++;
-//     }
-//     res.push(sum/k)
-//   }
-
-//   return res;
-// }
-
-
-function average(arr, k){
-  const result = [];
-  let sum = 0;
+function averageElements(arr, k){
+  let average = [];
   let left = 0;
+  let sum = 0;
 
-  for(let right=0; right < arr.length; right++ ){
-    sum = sum+arr[right];
-    if(right >= k-1){
-      // arrived at the end of the window
-      result.push(sum/k);
-      sum = sum - arr[right]; // subtract the element going out;
-      left = left + 1; 
+  for(let right=0; right<arr.length; right++){
+    sum = arr[right] + sum; 
+    if(right+1 >= k){
+      average.push(sum/k);
+      sum = sum - arr[left];
+      left = left + 1;
     }
   }
 
-  return result;
+  return average;
 }
 
-console.log(average([1, 3, 2, 6, -1, 4, 1, 8, 2], 5))
+console.log(averageElements([1, 3, 2, 6, -1, 4, 1, 8, 2], 5));
