316b1c8d2694f77030d6f6afaeff10b566f3f36a
Salah Osman <cvhashim04@gmail.com>
2022-06-10 23:18:56 -0500

    smallest subarray redo

--- 1-sliding-window/2-smallest-subarray.js
+++ 1-sliding-window/2-smallest-subarray.js
@@ -15,57 +15,25 @@ Output: 3
 Explanation: Smallest subarrays with a sum greater than or equal to '8' are [3, 4, 1] 
 or [1, 1, 6].
 **/ 
-function smallestSubarrayGreaterSum(arr, s){
-  let windowStart = 0;
+
+// function 
+function smallestSub(arr, testSum){
+  if(arr.length === 0) return 0;
+  let left = 0;
   let windowSum = 0;
-  let length = 0;
+  let lengthSubarray = Infinity;
 
-  for(let windowEnd=0; windowEnd<arr.length; windowEnd++){
-    // First get the sum for current window
-    windowSum = windowSum + arr[windowEnd];
-    length++;
-    console.log(windowSum)
-    if(windowSum >= s){
-      // remove start element from window
-      windowSum = windowSum - arr[windowStart] // 8 - 2
-      windowStart++;
-      length--;
-    }
-  }
-  // could not get to sum with given numbers
-  if(windowSum < s){
-    return 0;
-  }
-  return length;
-}
-
-// console.log(smallestSubarrayGreaterSum([2, 1, 5, 2, 8], 10000))
-
-
-
-// ========= Solution 2 ==============
-function smallest_subarray_sum(s, arr) {
-  let windowSum = 0,
-    minLength = Infinity,
-    windowStart = 0;
-
-  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-    windowSum += arr[windowEnd]; // add the next element
-    // shrink the window as small as possible until the 'window_sum' is smaller than 's'
-    while (windowSum >= s) {
-      minLength = Math.min(minLength, windowEnd - windowStart + 1);
-      windowSum -= arr[windowStart];
-      windowStart += 1;
+  for(let right=0; right < arr.length; right++){
+    windowSum = windowSum + arr[right]; // 1 + 5
+    while(windowSum >= testSum){
+      let windowLength = (right-left)+1; // get current window length 
+      lengthSubarray = Math.min(windowLength, lengthSubarray); // 3
+      windowSum = windowSum - arr[left];
+      left=left+1 // move left to the right by one
     }
   }
 
-  if (minLength === Infinity) {
-    return 0;
-  }
-  return minLength;
+  return lengthSubarray;
 }
 
-
-console.log(`Smallest subarray length: ` + smallest_subarray_sum(1, [2, 1, 5, 2, 3, 2]));
-// console.log(`Smallest subarray length: ` + smallest_subarray_sum(7, [2, 1, 5, 2, 8]));
-// console.log(`Smallest subarray length: ` + smallest_subarray_sum(8, [3, 4, 1, 1, 6]));
+console.log(smallestSub([2, 1, 5, 2, 8], 7))
