209d5e74952e44247868a62e5059ad222cb15d7a
Salah Osman <cvhashim04@gmail.com>
2023-03-06 22:42:46 -0600

    wip

--- 02-two-pointers/1-pair-target-sum.js
+++ 02-two-pointers/1-pair-target-sum.js
@@ -1,16 +1,6 @@
 // Easy
 // ============ Pair with Target Sum ==============
 
-/* 
-Input: [1, 2, 3, 4, 6], target=6
-Output: [1, 3]
-Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
-
-Input: [2, 5, 9, 11], target=11
-Output: [0, 2]
-Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11
-**/
-
 function pairSum(arr, target){
   let left = 0;
   let right = arr.length-1;
@@ -24,16 +14,16 @@ function pairSum(arr, target){
 
     if(val > target){
       right = right - 1;
-    }
-
-    if(val < target){
+    }else {
       left = left + 1;
     }
   }
 
-  return null;
+  return [-1,-1];
 }
 
 console.log(pairSum([2, 5, 9, 11], 11));
 
-// choice choice choice
+// Time complexity O(n)
+
+// Space complexity O(1)
