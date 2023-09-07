f64d8ae7f903559f96b1151b7b7f7e0889861fc6
Salah Osman <cvhashim04@gmail.com>
2023-09-06 21:04:25 -0500

    palindrome

---
+++ 00-warm-up/5-valid-palindrome.js
@@ -0,0 +1,41 @@
+// Easy 
+// ============ Valid Palindrome ==============
+
+// Input: sentence = "A man, a plan, a canal, Panama!"
+// true 
+
+// Two pointer solution
+
+function validPalindrome(str){
+  // let arr = str.split('');
+  let left = 0;
+  let right = str.length-1;
+
+  while(left < right){
+    // while char is not alphanumeric move to next line
+    while(!str[left].match(/[a-zA-Z0-9]/)){
+      // move up
+      left++;
+    }
+
+    while(!str[right].match(/[a-zA-Z0-9]/)){
+      // move down
+      right--;
+    }
+
+    if(str[left].toLowerCase() !== str[right].toLowerCase()){
+      return false;
+    }
+
+    left++;
+    right--;
+
+  }
+
+  return true;
+}
+
+console.log(validPalindrome('A man, a plan, a canal, Panama!'));
+
+// Time complexity O(n)
+// Space Complexity is O(1)
